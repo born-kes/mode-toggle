fdb2dac9fd0ffa6a516ba66019be0e92e6dd89ec
bornkes <born-kes@o2.pl>
2021-01-30 19:14:03 +0100

    refactoring for clean code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
         <App />
-    </div>
- , document.getElementById('root') );
-
-// serviceWorker.unregister(); // Disable to work offline. This can make it difficult to remove errors
-// serviceWorker.register(); //The application will work without internet, but you cannot debug
+    </div>,
+    document.getElementById('root') );
