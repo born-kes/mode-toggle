25e2cbced7eac2a11c5cd360f2a87ff34ae86306
bornkes <born-kes@o2.pl>
2022-10-15 18:21:33 +0200

    add use localStorage for color remember

--- index.js
+++ index.js
@@ -2,28 +2,35 @@
     let trans = () => {
         document.documentElement.classList.add('transition');
         window.setTimeout(() => {
-            document.documentElement.classList.remove('transition')
+            document.documentElement.classList.remove('transition');
         }, 1000)
     };
 
     const handlerChange = (el) => {
 
         if(el.target.checked) {
-            trans()
-            document.documentElement.setAttribute('data-theme', 'dark')
+            trans();
+            localStorage.setItem("theme", 'dark');
+            document.documentElement.setAttribute('data-theme', 'dark');
         } else {
-            trans()
-            document.documentElement.setAttribute('data-theme', 'light')
+            trans();
+            localStorage.setItem("theme", 'light');
+            document.documentElement.setAttribute('data-theme', 'light');
         }
     };
 
 document.getElementById('toggleMode').addEventListener('change',handlerChange);
-/*
-    return (
-        <div className={'toggle-container'}>
-            <input type="checkbox" id="toggleMode" onChange={handlerChange} />
-            <label htmlFor="toggleMode" />
-            <div />
-        </div>
-    );
-*/
+
+
+    let theme = localStorage.getItem("theme");
+    if(theme === 'dark'){
+            document.documentElement.setAttribute('data-theme', 'dark');
+            document.getElementById('toggleMode').checked = true;
+    } else if(theme === 'light') {
+            document.documentElement.setAttribute('data-theme', 'light');
+    } else if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            document.getElementById('toggleMode').checked = true; 
+
+    }
+console.log(theme, window.matchMedia('(prefers-color-scheme: dark)').matches)
